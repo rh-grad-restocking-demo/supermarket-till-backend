e2ac350e6447b342e9c761a8d367fff2af5ce6b8
Max Kossatz <max@kossatzonline.com>
2021-07-26 20:01:19 +0100

    removing everything to prepare for replacing it with a Camel K integration

--- till/application/purchase_items_request_dto.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from dataclasses import dataclass
-
-from till.application.item_dto import ItemDTO
-
-
-@dataclass(frozen=True)
-class PurchaseItemsRequestDTO:
-    items_purchased: List[ItemDTO]
