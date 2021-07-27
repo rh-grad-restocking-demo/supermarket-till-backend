01bec136d7f82d8b9bac1e98b5b01a7c2aacf43e
Max Kossatz <max@kossatzonline.com>
2021-07-27 12:33:14 +0100

    changed route name

--- TillBackend.java
+++ TillBackend.java
@@ -6,11 +6,25 @@ public class TillBackend extends RouteBuilder {
   @Override
   public void configure() throws Exception {
 
-      rest("/")
-        .post("/items_purchase").consumes("application/json").to("direct:items_purchase");
 
-      from("direct:items_purchase")
-        .to("amqp:queue:itemsPurchasedAddress");
+      rest("/")
+        .post("/product_purchase").consumes("application/json").to("direct:product_purchase");
+
+        
+
+
+      rest("/")
+          .verb("OPTIONS", "/").route()
+          .setHeader("Access-Control-Allow-Origin", constant("*"))
+          .setHeader("Access-Control-Allow-Methods", constant("GET, HEAD, POST,PUT, DELETE, TRACE, OPTIONS, CONNECT, PATCH"))
+          .setHeader("Access-Control-Allow-Headers", constant("Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"))
+          .setHeader("Allow", constant("GET, OPTIONS, POST, PATCH"));
+
+      from("direct:product_purchase")
+        .to("amqp:queue:productPurchasedAddress");
+
+
+
 
   }
 }
